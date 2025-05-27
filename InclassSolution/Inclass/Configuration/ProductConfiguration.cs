ed4ea33b536230e819d51106e7e400826326ae05
Ibrahim <Ibrahim@LAPTOP-L47MA11A>
2025-05-27 18:23:18 +0400

    Add project files.

---
+++ InclassSolution/Inclass/Configuration/ProductConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Inclass.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Inclass.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Name).IsRequired();
+            builder.Property(p => p.Description).IsRequired(false);
+            builder.Property(p=>p.Price).IsRequired().HasPrecision(10,3);
+        }
+    }
+}
---
+++ InclassSolution/Inclass/Context/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Inclass.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inclass.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+        
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
